24bf0f2053541b99dc13a3e01b93e4deb6ffaca5
mrtn.tome <mrtn.tome@gmail.com>
2020-07-13 18:51:17 -0300

    better test script

--- testing/test.sh
+++ testing/test.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env sh
 
+echo "Building site"
 docker run --rm -it -v $(pwd)/..:/src klakegg/hugo:0.73.0 && docker-compose up -d
+
+echo "Opening site"
+xdg-open http://localhost:1313
+
+echo "Press [ENTER] to terminate"
+while true
+do
+read -s -N 1 -t 1 key
+if [[ $key == $'\x0a' ]];
+then
+docker-compose down && printf "Bye"
+exit 0
+fi
+done
+
